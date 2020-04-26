52c7f21e6f1a596e0440b53f6ab8940211db835f
feras ammouri <m.feras.ammouri@gmail.com>
2020-04-26 14:17:48 +0200

    Add project files.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel model = connection.CreateModel();
+            model.QueueDeclare("BasicTest", false, false, false, null);
+            EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(model);
+            eventingBasicConsumer.Received += eventingBasicConsumer_Received;
+            model.BasicConsume("BasicTest", true, eventingBasicConsumer);
+            Console.ReadLine();
+        }
+
+        private static void eventingBasicConsumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            byte[] body = e.Body;
+            string message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel model = connection.CreateModel();
+            model.QueueDeclare("BasicTest", false, false, false, null);
+            string message = "Getting started with RabbitMQ .Net core";
+            byte[] body = Encoding.UTF8.GetBytes(message);
+            model.BasicPublish("", "BasicTest", null, body);
+
+            Console.WriteLine($"Sent message {0} ...", message);
+
+            Console.ReadLine();
+        }
+    }
+}
