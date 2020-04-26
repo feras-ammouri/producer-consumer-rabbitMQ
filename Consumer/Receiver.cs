31413475e67dd820326d6f56aaa4cf69934f2ceb
feras ammouri <m.feras.ammouri@gmail.com>
2020-04-26 16:01:15 +0200

    #Initial commit

--- Consumer/Receiver.cs
+++ Consumer/Receiver.cs
@@ -9,25 +9,38 @@ namespace Consumer
     {
         public static void Main(string[] args)
         {
+            // Instantiate a connection to the RabbitMQ node
             ConnectionFactory connectionFactory = new ConnectionFactory()
             {
                 HostName = "localhost"
             };
 
-            IConnection connection = connectionFactory.CreateConnection();
-            IModel model = connection.CreateModel();
-            model.QueueDeclare("BasicTest", false, false, false, null);
-            EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(model);
-            eventingBasicConsumer.Received += eventingBasicConsumer_Received;
-            model.BasicConsume("BasicTest", true, eventingBasicConsumer);
-            Console.ReadLine();
-        }
+            // Open a connection
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                // Open a channel
+                using (IModel model = connection.CreateModel())
+                {
+                    // Declare a queue
+                    model.QueueDeclare("Queue", false, false, false, null);
 
-        private static void eventingBasicConsumer_Received(object sender, BasicDeliverEventArgs e)
-        {
-            byte[] body = e.Body;
-            string message = Encoding.UTF8.GetString(body);
-            Console.WriteLine(message);
+                    // Create a consumer object
+                    EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(model);
+
+                    // Event for dispatching deliveries and other consumer lifecycle
+                    eventingBasicConsumer.Received += (channel, ea) =>
+                    {
+                        byte[] body = ea.Body;
+                        string message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Received message {0}...",message);
+                    };
+
+                    // Use the channel to consume the message 
+                    model.BasicConsume("Queue", true, eventingBasicConsumer);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- Producer/Sender.cs
+++ Producer/Sender.cs
@@ -8,20 +8,33 @@ namespace Producer
     {
         public static void Main(string[] args)
         {
+            // Instantiate a connection to the RabbitMQ node
             ConnectionFactory connectionFactory = new ConnectionFactory()
             {
                 HostName = "localhost"
             };
 
-            IConnection connection = connectionFactory.CreateConnection();
-            IModel model = connection.CreateModel();
-            model.QueueDeclare("BasicTest", false, false, false, null);
-            string message = "Getting started with RabbitMQ .Net core";
-            byte[] body = Encoding.UTF8.GetBytes(message);
-            model.BasicPublish("", "BasicTest", null, body);
+            // Open a connection
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                // Open a channel
+                using (IModel model = connection.CreateModel())
+                {
+                    // Declare a queue
+                    model.QueueDeclare("Queue", false, false, false, null);
 
-            Console.WriteLine($"Sent message {0} ...", message);
+                    // Create a message 
+                    string message = "RabbitMQ .Net core";
+                    byte[] body = Encoding.UTF8.GetBytes(message);
 
+                    // Publish the message 
+                    model.BasicPublish("", "Queue", null, body);
+
+                    Console.WriteLine("Sent message {0} ...", message);
+                }
+            }
+
+            Console.WriteLine("Press [enter] to exit the sender App...");
             Console.ReadLine();
         }
     }
